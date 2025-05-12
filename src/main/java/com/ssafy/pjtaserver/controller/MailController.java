88e609d83d7d64a562838c3869cc9ae6c7366f46
dohyeon <163515795+dojin8351@users.noreply.github.com>
2025-05-12 14:30:54 +0900

    feat:Email 기능 제작 - 이메일 전송기능 제작

--- src/main/java/com/ssafy/pjtaserver/controller/MailController.java
+++ src/main/java/com/ssafy/pjtaserver/controller/MailController.java
@@ -1,11 +1,12 @@
 package com.ssafy.pjtaserver.controller;
 
 import com.ssafy.pjtaserver.dto.MailDto;
+import com.ssafy.pjtaserver.enums.ApiResponseCode;
 import com.ssafy.pjtaserver.service.mail.MailService;
+import com.ssafy.pjtaserver.util.ApiResponse;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,9 +35,14 @@ public class MailController {
      * @return
      */
     @PostMapping("/html-email")
-    public ResponseEntity<Object> sendHtmlEmail(@RequestBody MailDto mailDto) throws MessagingException {
-        mailService.sendEmail(mailDto);
-        return new ResponseEntity<>(null, HttpStatus.OK);
-    }
+    public ResponseEntity<ApiResponse> sendHtmlEmail(@RequestBody MailDto mailDto){
+        boolean isSend = mailService.sendEmail(mailDto);
 
+        if (isSend) {
+            return ApiResponse.of(ApiResponseCode.SUCCESS);
+        } else {
+            log.error("이메일 전송 실패 대상 이메일: {}", mailDto.getEmailAddr());
+            return ApiResponse.of(ApiResponseCode.INVALID_REQUEST);
+        }
+    }
 }
