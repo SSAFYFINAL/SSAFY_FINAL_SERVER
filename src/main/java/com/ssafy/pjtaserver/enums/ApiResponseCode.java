cb36f0a94b4913ec00a7c71fcbd2e627d6c92de6
dohyeon <163515795+dojin8351@users.noreply.github.com>
2025-05-15 00:08:45 +0900

    chore : 응답 코드 추가

--- src/main/java/com/ssafy/pjtaserver/enums/ApiResponseCode.java
+++ src/main/java/com/ssafy/pjtaserver/enums/ApiResponseCode.java
@@ -9,6 +9,8 @@ public enum ApiResponseCode {
     SUCCESS(HttpStatus.OK, 100, "요청이 정상적으로 처리되었습니다."),
     USER_CREATED(HttpStatus.CREATED, 101, "회원가입이 완료되었습니다."),
     LOGIN_SUCCESS(HttpStatus.OK, 102, "로그인에 성공했습니다."),
+    EMAIL_SEND_SUCCESS(HttpStatus.OK, 103, "이메일이 정상적으로 발송되었습니다."),
+    EMAIL_VERIFY_SUCCESS(HttpStatus.OK, 100, "이메일 인증이 정상적으로 처리되었습니다."),
 
     // 실패
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, 400, "잘못된 요청입니다."),
@@ -17,6 +19,9 @@ public enum ApiResponseCode {
     FORBIDDEN(HttpStatus.FORBIDDEN, 403, "권한이 없습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, 404, "요청한 리소스를 찾을 수 없습니다."),
     USER_CREATED_ERROR(HttpStatus.BAD_REQUEST,401,"회원가입 실패"),
+
+    EMAIL_SEND_ERROR(HttpStatus.FORBIDDEN,403,"이메일 인증 휫수 초과."),
+    EMAIL_VERIFY_ERROR(HttpStatus.FORBIDDEN, 403, "이메일 인증에 실패하였습니다."),
 
     // 토큰 에러
     ERROR_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, 401, "ERROR_ACCESS_TOKEN"),
