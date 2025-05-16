14fa8f6eadc9586290d1844f61480740d7e8c817
dohyeon <163515795+dojin8351@users.noreply.github.com>
2025-05-16 12:47:27 +0900

    SMS-51 feat : bookDetail관련 컨트롤러, 반환 dto 정의

---
+++ src/main/java/com/ssafy/pjtaserver/controller/book/ApiBookAuthController.java
@@ -0,0 +1,17 @@
+package com.ssafy.pjtaserver.controller.book;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/auth/book")
+public class ApiBookAuthController {
+
+    // 대출 요청하는 로직ㅗ
+
+    // 찜하기 로직 db 수정 필요해보인다.
+}
