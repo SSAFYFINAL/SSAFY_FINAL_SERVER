54a7c2a812246b7940ec4ec20ae962f60ea9dab3
dohyeon <163515795+dojin8351@users.noreply.github.com>
2025-05-16 12:47:35 +0900

    SMS-51 feat : bookDetail관련 서비스 정의

--- src/main/java/com/ssafy/pjtaserver/service/book/BookService.java
+++ src/main/java/com/ssafy/pjtaserver/service/book/BookService.java
@@ -1,10 +1,14 @@
 package com.ssafy.pjtaserver.service.book;
 
+import com.ssafy.pjtaserver.dto.response.book.BookDetailDto;
 import com.ssafy.pjtaserver.dto.response.book.BookInfoSearchCondition;
 import com.ssafy.pjtaserver.dto.response.book.BookInfoSearchDto;
 import com.ssafy.pjtaserver.dto.response.book.PageResponseDto;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface BookService {
     PageResponseDto<BookInfoSearchDto> searchPageComplex(BookInfoSearchCondition condition, Pageable updatedPageable);
+    BookDetailDto getDetail(Long bookInfoId);
 }
