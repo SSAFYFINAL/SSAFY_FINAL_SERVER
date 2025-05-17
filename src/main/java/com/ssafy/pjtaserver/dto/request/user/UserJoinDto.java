984376e38dd3677020653eef876c5433caf646fa
dohyeon <163515795+dojin8351@users.noreply.github.com>
2025-05-17 18:04:54 +0900

    SMS-12 chore : user 관련 dto, entity 검증, 이름 수정

--- src/main/java/com/ssafy/pjtaserver/dto/request/user/UserJoinDto.java
+++ src/main/java/com/ssafy/pjtaserver/dto/request/user/UserJoinDto.java
@@ -8,10 +8,20 @@ import lombok.Data;
 
 @Data
 public class UserJoinDto {
+
     @NotBlank(message = "userLoginId는 필수값 입니다.")
+    @Pattern(
+            regexp = "^[a-zA-Z][a-zA-Z0-9]{3,15}$",
+            message = "아이디는 영문자로 시작하며, 영문자와 숫자를 포함한 4~16자여야 합니다."
+    )
     private String userLoginId;
 
+
     @NotBlank(message = "userPwd는 필수값 입니다.")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[!@#$%^&*()])(?=.*[a-zA-Z]).{8,}$",
+            message = "비밀번호는 최소 8자 이상이며, 숫자와 특수 문자, 영문자를 포함해야 합니다."
+    )
     private String userPwd;
 
     @NotBlank(message = "checkUserPwd는 필수값 입니다.")
@@ -28,7 +38,10 @@ public class UserJoinDto {
     private String userEmail;
 
     @NotBlank(message = "userPhone는 필수값 입니다.")
-    @Pattern(regexp = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$")
+    @Pattern(
+            regexp = "^010\\d{4}\\d{4}$",
+            message = "전화번호는 010으로 시작하고 8자리 숫자로만 구성되어야 합니다."
+    )
     private String userPhone;
 
     @NotNull(message = "isCheckedPw는 필수값 입니다.")
