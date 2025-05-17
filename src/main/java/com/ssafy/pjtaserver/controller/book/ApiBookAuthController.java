7ed86fae5736cbe84354840c3047b0406c782e99
dohyeon <163515795+dojin8351@users.noreply.github.com>
2025-05-17 12:07:02 +0900

    SMS-28 feat : 찜 추가 기능 관련 컨트롤러 정의

--- src/main/java/com/ssafy/pjtaserver/controller/book/ApiBookAuthController.java
+++ src/main/java/com/ssafy/pjtaserver/controller/book/ApiBookAuthController.java
@@ -1,7 +1,15 @@
 package com.ssafy.pjtaserver.controller.book;
 
+import com.ssafy.pjtaserver.enums.ApiResponseCode;
+import com.ssafy.pjtaserver.service.book.BookService;
+import com.ssafy.pjtaserver.util.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth/book")
 public class ApiBookAuthController {
 
-    // 대출 요청하는 로직ㅗ
+    private final BookService bookService;
+    // 대출 요청하는 로직
 
     // 찜하기 로직 db 수정 필요해보인다.
+    @PostMapping("/like/{bookInfoId}")
+    public ResponseEntity<ApiResponse> likeBook(@AuthenticationPrincipal UserDetails userDetails, @PathVariable("bookInfoId") Long bookInfoId) {
+        String userLoginId = userDetails.getUsername();
+        log.info("------------------------------api like book------------------------------");
+
+        if(!bookService.addFavoriteBook(userLoginId, bookInfoId)) {
+            return ApiResponse.of(ApiResponseCode.INVALID_REQUEST);
+        }
+
+        return ApiResponse.of(ApiResponseCode.SUCCESS);
+    }
 }
