ddd3b6253721485a33716db4c7948d4a5d027257
dohyeon <163515795+dojin8351@users.noreply.github.com>
2025-05-27 22:13:43 +0900

    hotfix : 더미데이터 수정

--- src/main/java/com/ssafy/pjtaserver/DummyData2.java
+++ src/main/java/com/ssafy/pjtaserver/DummyData2.java
@@ -40,16 +40,16 @@ public class DummyData2 implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
 //         유저 등록
 //        List<User> userList = new ArrayList<>();
-//        User user1 = User.createNormalUser("dojin9654", passwordEncoder.encode("!ehgus12"), "김도현", "dojin8351", "dojin8351@gmail.com", "01085392392");
-//        User user2 = User.createNormalUser("joohyun0913", passwordEncoder.encode("!ehgus12"), "권주현", "소개팅만3번째", "joohyun0913@naver.com", "01065111647");
-//        User user3 = User.createNormalUser("yoonjeongKm", passwordEncoder.encode("!ehgus12"), "김윤정", "설윤 정", "yoommnj@gmail.com", "01032571477");
-//        User user4 = User.createNormalUser("bkw0820", passwordEncoder.encode("!ehgus12"), "변가원", "응가", "bkw0820@gmail.com", "01025047113");
-//        User user5 = User.createNormalUser("woqls1009", passwordEncoder.encode("!ehgus12"), "이재빈", "재빈퐁", "woqls1009@daum.net", "01055474833");
-//        User user6 = User.createNormalUser("hyunji1132", passwordEncoder.encode("!ehgus12"), "이현지", "올해31", "hyunji1132@gmail.com", "01048168496");
-//        User user7 = User.createNormalUser("juywoo", passwordEncoder.encode("!ehgus12"), "주연우", "슈웃~", "juywoo@naver.com", "01028202675");
-//        User user8 = User.createNormalUser("istory1997", passwordEncoder.encode("!ehgus12"), "이언지", "짝짝이소녀", "istory1997@naver.com", "01094660152");
-//        User user9 = User.createNormalUser("junho9661", passwordEncoder.encode("!ehgus12"), "문준호", "당구는어려워..", "junho9661@gmail.com", "01059266632");
-//        User user10 = User.createNormalUser("dsazxc035", passwordEncoder.encode("!ehgus12"), "김현재", "언지야 나 그만좋아해", "dsazxc035@naver.com", "01062563914");
+//        User user1 = User.createNormalUser("dojin9654", passwordEncoder.encode("!ehgus12"), "김도현", "dojin8351", "dojin8351@gmail.com", "01011111111");
+//        User user2 = User.createNormalUser("joohyun0913", passwordEncoder.encode("!ehgus12"), "권주현", "소개팅만3번째", "joohyun09132@naver.com", "01022222222");
+//        User user3 = User.createNormalUser("yoonjeongKm", passwordEncoder.encode("!ehgus12"), "김윤정", "설윤 정", "yoommnj2@gmail.com", "01033333333");
+//        User user4 = User.createNormalUser("bkw0820", passwordEncoder.encode("!ehgus12"), "변가원", "응가", "bkw08202@gmail.com", "01044444444");
+//        User user5 = User.createNormalUser("woqls1009", passwordEncoder.encode("!ehgus12"), "이재빈", "재빈퐁", "woqls10093@daum.net", "01055555555");
+//        User user6 = User.createNormalUser("hyunji1132", passwordEncoder.encode("!ehgus12"), "이현지", "올해31", "hyunji11324@gmail.com", "01066666666");
+//        User user7 = User.createNormalUser("juywoo", passwordEncoder.encode("!ehgus12"), "주연우", "슈웃~", "juywoo5@naver.com", "01077777777");
+//        User user8 = User.createNormalUser("istory1997", passwordEncoder.encode("!ehgus12"), "이언지", "짝짝이소녀", "istory19971@naver.com", "01088888888");
+//        User user9 = User.createNormalUser("junho9661", passwordEncoder.encode("!ehgus12"), "문준호", "당구는어려워..", "junho96612@gmail.com", "01099999999");
+//        User user10 = User.createNormalUser("dsazxc035", passwordEncoder.encode("!ehgus12"), "김현재", "언지야 나 그만좋아해", "dsazxc0356@naver.com", "01000000000");
 //
 //        userList.add(user1);
 //        userList.add(user2);
